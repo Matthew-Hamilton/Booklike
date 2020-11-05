6e234e6840376d0c6664c3808db66b3e1188b070
MHamilton <32247409+cyberboy379@users.noreply.github.com>
2020-11-05 13:47:23 +0000

    Basic Bullet Destruction & left side cam works

---
+++ BookLike/Assets/Scripts/GameCam.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCam : MonoBehaviour
+{
+    float xOffset = 0;
+    float yOffset = 0;
+    Camera thisCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisCamera = this.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("xMin: " + thisCamera.rect.xMin);
+        //Debug.Log(thisCamera.ScreenToWorldPoint(Vector3.zero));
+        
+    }
+
+    public void beUpdated()
+    {
+        if (thisCamera.ScreenToWorldPoint(Vector3.zero).x < -9.5)
+        {
+            xOffset += -9.5f - (float)thisCamera.ScreenToWorldPoint(Vector3.zero).x;
+            thisCamera.transform.position += new Vector3(-9.5f - (float)thisCamera.ScreenToWorldPoint(Vector3.zero).x, 0, 0);
+        }
+        else
+        {
+            if(xOffset > 0)
+            {
+                float tempOffset = Mathf.Min(-9.5f - thisCamera.ScreenToWorldPoint(Vector3.zero).x, xOffset);
+                thisCamera.transform.position += new Vector3(tempOffset, 0, 0);
+                xOffset += tempOffset;
+                if (xOffset < 0)
+                    xOffset = 0;
+            }
+
+        }
+        Debug.Log(xOffset);
+    }
+}
--- BookLike/Assets/Scripts/ProjectileMovement.cs
+++ BookLike/Assets/Scripts/ProjectileMovement.cs
@@ -4,22 +4,37 @@ using UnityEngine;
 
 public class ProjectileMovement : MonoBehaviour
 {
+    public Sprite wallSprite;
     public GameObject parent;
     Vector3 velocity;
-    float speed = 5;
+    float lifetime = 1;
+    float speed = 4.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         if(parent.GetComponent<PlayerMovement>())
         {
-            velocity = new Vector3(parent.GetComponent<PlayerMovement>().GetWeaponDirection().x * speed, parent.GetComponent<PlayerMovement>().GetWeaponDirection().y * speed) + (Vector3)(parent.GetComponent<PlayerMovement>().GetVelocity());
+            PlayerMovement pMove = parent.GetComponent<PlayerMovement>();
+            velocity = new Vector3(pMove.GetWeaponDirection().x * speed, pMove.GetWeaponDirection().y * speed);
         }
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(lifetime <= 0)
+        {
+            Destroy(gameObject);
+        }
         transform.position += velocity * Time.deltaTime;
+        lifetime -= Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Wall"))
+            Destroy(gameObject);
     }
 }
